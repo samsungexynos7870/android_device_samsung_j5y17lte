f8565a4c1ed72f9e455a4d4700eff22d132fa43c
Alejandro <apamarin@gmail.com>
2020-07-06 00:18:23 +0200

    j5y17lte: AEX changes

--- aosp_j5y17lte.mk
+++ aosp_j5y17lte.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/j5y17lte/device.mk)
 
 # Inherit some common Aosp stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j5y17lte
