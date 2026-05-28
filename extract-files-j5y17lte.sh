699e8e8cfab94c7be9f140d89d1dee05c1cdc2bb
Flominator <github.flominatorgd@gmail.com>
2026-05-29 01:44:20 +0200

    j5y17lte: update proprietary-files and scripts

---
+++ extract-files-j5y17lte.sh
@@ -0,0 +1,43 @@
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+ORIGINAL_DIR="${PWD}"
+
+ANDROID_ROOT="${MY_DIR}/../../.."
+DEVICE_COMMON=universal7870-common
+VENDOR=samsung
+VENDOR_MK_ROOT="${ANDROID_ROOT}"/vendor/"${VENDOR}"
+DEVICE_COMMON_ROOT="${ANDROID_ROOT}"/device/"${VENDOR}"/"${DEVICE_COMMON}"
+
+TARGET_SOURCES_DIR="${VENDOR_MK_ROOT}/tmp/sources"
+mkdir -p "$TARGET_SOURCES_DIR"
+
+REPO_URLS=(
+    "https://github.com/Exynos7870-labs/samsung_a6eltemtr_dump.git -b a6eltemtr-user-10-QP1A.190711.020-A600T1UVS8CUA1-release-keys A600T1UVS8CUA1"
+    "https://github.com/Exynos7870-labs/samsung_j5y17lte_dump.git -b j5y17ltexx-user-9-PPR1.180610.011-J530FXXS8CUE4-release-keys J530FXXS8CUE4"
+)
+
+cd "$TARGET_SOURCES_DIR"
+for i in "${!REPO_URLS[@]}"; do
+    # Extract target directory name (last word in the string)
+    repo_info="${REPO_URLS[$i]}"
+    target_dir="${repo_info##* }"
+    
+    echo "Checking: $target_dir"
+    
+    if [[ -d "$target_dir" ]]; then
+        echo "  Directory $target_dir already exists. Skipping."
+    else
+        echo "  Cloning: ${REPO_URLS[$i]}"
+        git clone ${REPO_URLS[$i]}
+    fi
+done
+ls
+cd "$ORIGINAL_DIR"
+
+COMMON_Q_A6ELTE_PATH="${TARGET_SOURCES_DIR}/A600T1UVS8CUA1"
+COMMON_P_J5Y17LTE_PATH="${TARGET_SOURCES_DIR}/J530FXXS8CUE4"
+
+# files
+./extract-files.sh j5y17lte vendor-tools/proprietary-files_a6elte.txt -n -k $COMMON_Q_A6ELTE_PATH
+./extract-files.sh j5y17lte vendor-tools/proprietary-files_j5y17lte.txt -n -k $COMMON_P_J5Y17LTE_PATH
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -26,9 +26,9 @@ export INITIAL_COPYRIGHT_YEAR=2017
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="${MY_DIR}/../../.."
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -36,13 +36,19 @@ fi
 source "${HELPER}"
 
 # Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}"
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 
 # Copyright headers and guards
 write_headers "j5y17lte"
 
+mkdir -p "${MY_DIR}/vendor-tools/unified_proprietary"
+
+cat ${MY_DIR}/vendor-tools/*.txt > "${MY_DIR}/vendor-tools/unified_proprietary/proprietary-files.txt"
+
 # The standard blobs
-write_makefiles "${MY_DIR}/proprietary-files.txt" true
+write_makefiles "${MY_DIR}/vendor-tools/unified_proprietary/proprietary-files.txt" true
+
+rm -rf "${MY_DIR}/vendor-tools/unified_proprietary"
 
 # Finish
 write_footers
