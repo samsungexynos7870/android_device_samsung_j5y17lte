3ce7d278621901f5464e8fba466f3a3362395012
Flominator <github.flominatorgd@gmail.com>
2026-07-04 19:08:07 +0200

    j5y17lte: setup oreo camera
    pie camera stream keeps crashing with oss implementation

--- extract-files-j5y17lte.sh
+++ extract-files-j5y17lte.sh
@@ -15,6 +15,7 @@ mkdir -p "$TARGET_SOURCES_DIR"
 REPO_URLS=(
     "https://github.com/Exynos7870-labs/samsung_a6eltemtr_dump.git -b a6eltemtr-user-10-QP1A.190711.020-A600T1UVS8CUA1-release-keys A600T1UVS8CUA1"
     "https://github.com/Exynos7870-labs/samsung_j5y17lte_dump.git -b j5y17ltexx-user-9-PPR1.180610.011-J530FXXS8CUE4-release-keys J530FXXS8CUE4"
+    "https://github.com/Exynos7870-labs/samsung_j5y17lte_dump.git -b j5y17ltexx-user-8.1.0-M1AJQ-J530FXXS5BSE3-release-keys J530FXXS5BSE3"
 )
 
 cd "$TARGET_SOURCES_DIR"
@@ -37,7 +38,9 @@ cd "$ORIGINAL_DIR"
 
 COMMON_Q_A6ELTE_PATH="${TARGET_SOURCES_DIR}/A600T1UVS8CUA1"
 COMMON_P_J5Y17LTE_PATH="${TARGET_SOURCES_DIR}/J530FXXS8CUE4"
+COMMON_O_J5Y17LTE_PATH="${TARGET_SOURCES_DIR}/J530FXXS5BSE3"
 
 # files
 ./extract-files.sh j5y17lte vendor-tools/proprietary-files_a6elte.txt -n -k $COMMON_Q_A6ELTE_PATH
-./extract-files.sh j5y17lte vendor-tools/proprietary-files_j5y17lte.txt -n -k $COMMON_P_J5Y17LTE_PATH
+./extract-files.sh j5y17lte vendor-tools/proprietary-files_j5y17lte_p.txt -n -k $COMMON_P_J5Y17LTE_PATH
+./extract-files.sh j5y17lte vendor-tools/proprietary-files_j5y17lte_o.txt -n -k $COMMON_O_J5Y17LTE_PATH
